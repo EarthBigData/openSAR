6f48d6ae5afc5cfe9a80fd8eac15f214c988d628
Josef Kellndorfer <josef@earthbigdata.com>
2021-11-18 11:33:33 -0500

    updates with metadata writing

--- code/seppo/seppo_vis_makeRGBs.py
+++ code/seppo/seppo_vis_makeRGBs.py
@@ -11,6 +11,7 @@ Choose blue or red flags for two date images where second date is in the blue or
 
 import os,sys
 from osgeo import gdal
+import time
 import xarray as xr
 import pandas as pd
 import fsspec
@@ -72,8 +73,9 @@ def myargsparse(argv):
     p.add_argument("-pol","--polarizations",required=False,help='Polarizations',action='store',default=['vv','vh'],nargs='*',choices=('vv','vh'))
     p.add_argument("-red","--red",required=False,help='For two dates, assigns the first date in date list to red, duplicates 2nd date for blue band',action='store_true',default=False)
     p.add_argument("-blue","--blue",required=False,help='For two dates, assigns the first date in date list to blue, duplicates 2nd date for red band. Ignored when -red is set',action='store_true',default=False)
+    p.add_argument("-desc","--description",required=False,help='Description of Data Set e.g. "Sentinel-1 SAR Backscatter"',action='store',default=None)
     p.add_argument("-k","--keyfile",required=False,help='Path to key file to unlock credentials',action='store',default=os.path.join(os.environ['HOME'],'.ebd','ebd_key_wasabi_public.txt'))
-    p.add_argument("-debug","--debug",required=False,help='Debug mode, keeping data',action='store_true',default=False)
+    p.add_argument("-dryrun","--dryrun",required=False,help='Dryrun Mode. Shows selected Stack VRTs and selectded bands and dates.',action='store_true',default=False)
     p.add_argument("-v","--verbose",required=False,help='Verbose output',action='store_true',default=False)
 
     args=p.parse_args(argv[1:])
@@ -81,7 +83,7 @@ def myargsparse(argv):
     return args
 
 
-def make_geotiff(vrtfile,dates,outdir=None,red=False,blue=False):
+def make_geotiff(vrtfile,dates,outdir=None,red=False,blue=False,description=None,verbose=False,dryrun=False):
     # Open the vrtfile as data array
     if not vrtfile.startswith('/vsis3/'):
         vrtfile='/vsis3/'+vrtfile
@@ -115,36 +117,43 @@ def make_geotiff(vrtfile,dates,outdir=None,red=False,blue=False):
     else: 
         bands.append(bands[-1])
 
-    
-    print('Band order for RGB', bands)
+    band_dates=[str(da.time[x-1].values).split('T')[0] for x in bands]
 
-    # Make a list of vrts for each band first
-    input_file_list=[]
-    for b in bands:
-        name=f'/vsimem/{b}.vrt'
-        input_file_list.append(gdal.BuildVRT(name,vrtfile,bandList=[b],srcNodata=0,VRTNodata=0))
+    if verbose:
+        print('Band/Date order for Grayscale/RGB:', bands,band_dates)
 
-    subset_vrt='/vsimem/crap.vrt'
-
-    out=gdal.BuildVRT(subset_vrt,input_file_list,separate=True,srcNodata=0,VRTNodata=0)
-    out=None
-    for i in input_file_list:
-        i=None
-    input_file_list=None
-
-    # outname
-    basename=vrtfile.split('/')[-1].replace('.vrt','')
-    outname='_'.join([basename]+found_dates)+'.tif'
-    
-    if outdir:
-        os.makedirs(outdir,exist_ok=True)
-        outpath=os.path.join(outdir,outname)
+    if dryrun:
+        return None
     else:
-        outpath=os.path.join(os.getcwd(),outname)
-    out2 = gdal.Translate(outpath,subset_vrt)
-    out2 = None
-    
-    return outpath
+        # Make a list of vrts for each band first
+        input_file_list=[]
+        for b in bands:
+            name=f'/vsimem/{b}.vrt'
+            input_file_list.append(gdal.BuildVRT(name,vrtfile,bandList=[b],srcNodata=0,VRTNodata=0))
+
+        subset_vrt='/vsimem/crap.vrt'
+
+        out=gdal.BuildVRT(subset_vrt,input_file_list,separate=True,srcNodata=0,VRTNodata=0)
+        out=None
+        for i in input_file_list:
+            i=None
+        input_file_list=None
+
+        # outname
+        basename=vrtfile.split('/')[-1].replace('.vrt','')
+        outname='_'.join([basename]+found_dates)+'.tif'
+        
+        if outdir:
+            os.makedirs(outdir,exist_ok=True)
+            outpath=os.path.join(outdir,outname)
+        else:
+            outpath=os.path.join(os.getcwd(),outname)
+        out2 = gdal.Translate(outpath,subset_vrt)
+        out2 = None
+        # Set Metadata
+        _ = seppo_SetMetadata(outpath,band_names=band_dates,description=description)
+        
+        return outpath
 
 
 def make_filesystem(keyfile):
@@ -201,20 +210,79 @@ def make_vrtlist(args,filesystem):
     return vrtlist
 
 
+def seppo_SetMetadata(src_dataset,band_names=None,description=None):
+    '''Writes the seppo metadata to gdal file. If GTiff, sets the TIFFTAGs for DATETIME and SOFTWARE
+    src_dataset can be filename or and open gdal.Dataset handle
+    '''
+    try:    
+        if isinstance(src_dataset,str):
+            img_handle=gdal.Open(src_dataset,gdal.GA_Update)
+        elif isinstance(src_dataset,gdal.Dataset):
+            image_name=src_dataset.GetFileList()[0]
+            src_dataset=None
+            img_handle=gdal.Open(image_name,gdal.GA_Update)
+        else:
+            print('Cannot set  Metadata. Invalid type: {}'.format(src_dataset))
+            return
+        gdalformat=img_handle.GetDriver().ShortName
+        sw='TIFFTAG_SOFTWARE' if gdalformat.lower()=='gtiff' else 'SOFTWARE'
+        now = time.strftime('%Y-%m-%d %H:%M:%S UTC%z',time.localtime())
+        timestamp='TIFFTAG_DATETIME' if gdalformat.lower()=='gtiff' else 'DATETIME'
+        md = img_handle.GetMetadata()
+        md[sw]='SEPPO version 3.0.0'
+        md[timestamp]=now
+        if gdalformat.lower()=='gtiff':
+            md['TIFFTAG_ARTIST']='info@earthbigdata.com'
+            md['TIFFTAG_COPYRIGHT']='Earth Big Data LLC'
+            if description:
+                md['TIFFTAG_IMAGEDESCRIPTION']=description
+        else:
+            md['ARTIST']='Josef Kellndorfer, josef@earthbigdata.com'
+            md['COPYRIGHT']='Earth Big Data LLC'
+            if description:
+                md['IMAGEDESCRIPTION']=description
+
+        img_handle.SetMetadata(md)
+        if description:
+            img_handle.SetDescription(description)
+
+        # Write the band metadata
+        if band_names:
+            if len(band_names) != img_handle.RasterCount:
+                raise 'Mismatch in band nubmers and band labels'
+            else:
+                for idx in range(img_handle.RasterCount):
+                    band=img_handle.GetRasterBand(idx+1)
+                    bname=band_names[idx]
+                    md={'Date':bname}
+                    #print('Metadata',md)
+                    band.SetDescription(bname)
+                    band.SetMetadata(md)
+                    band=None
+        # Close or return handle
+        if isinstance(src_dataset,str):
+            img_handle=None
+        else:
+            src_dataset=img_handle
+    except Exception as e:
+        print('Exception in seppo_SetMetadata:',e)
+
 def processing(args):
 
     filesystem = make_filesystem(args.keyfile)
     vrtlist    = make_vrtlist(args,filesystem)
 
-    print('VRTLIST:')
-    for v in vrtlist:
-        print(v)
-    print()
+    if args.verbose:
+        print('VRTLIST:')
+        for v in vrtlist:
+            print(v)
+        print()
 
     for vrtfile in vrtlist:
-        print('\nWorking on',vrtfile)
-        geotiff = make_geotiff(vrtfile,args.dates,outdir=args.outdir,red=args.red,blue=args.blue)
-        print('Produced',geotiff)
+        print('\n***** Working on',vrtfile)
+        geotiff = make_geotiff(vrtfile,args.dates,outdir=args.outdir,red=args.red,blue=args.blue,description=args.description,verbose=args.verbose,dryrun=args.dryrun)
+        if geotiff:
+            print('Produced',geotiff)
     
 
 def main(a):
